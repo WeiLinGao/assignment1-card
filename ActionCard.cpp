7508526d47ca10afdf927dcb07eb24fa5d582dd3
Weilin Gao <136024634+WeiLinGao@users.noreply.github.com>
2023-11-03 15:45:04 -0400

    Update ActionCard.cpp

--- ActionCard.cpp
+++ ActionCard.cpp
@@ -6,44 +6,65 @@
 #include <cctype>
 #include <regex>
 #include "Card.hpp"
-#include "ActionCard.hpp"
-#include "Hand.hpp"
 
+class ActionCard : public Card
+{
+public:
+    /**
+     * @post: Construct a new Action Card object
+     */
+    ActionCard();
 
-ActionCard::ActionCard():Card()
+    /**
+     * @return true if the card is playable, false otherwise
+     * For a card to be playable, it has to be drawn and the instruction has to be valid
+     * Valid instructions:
+     * DRAW x CARD(S) : assume x is a positive integer
+     * PLAY x CARD(S) : assume x is a positive integer
+     * REVERSE HAND : reverse the order of the cards in the hand
+     * SWAP HAND WITH OPPONENT : swap the hand with the opponent
+     */
+    bool isPlayable() override;
+
+    /**
+     * @post: Print the ActionCard in the following format:
+     * Type: [CardType]
+     * Instruction: [Instruction]
+     * Card:
+     * [ImageData]
+     *
+     * Note: For [ImageData]: If there is no image data, print "No image data" instead
+     */
+    void Print() const override;
+};
+
+#endif
+
+ActionCard::ActionCard() : Card()
 {
     cardType_ = CardType::ACTION_CARD;
 }
 
-virtual bool ActionCard::isPlayable() override
+bool ActionCard::isPlayable()
 {
-
-
-    if (getDrawn())
-    {
+    if (getDrawn()) {
         std::string instruction = getInstruction();
         std::regex draw("^DRAW \\d+ CARD(S)?$");
         std::regex play("^PLAY \\d+ CARD(S)?$");
         std::regex reverse("^REVERSE HAND$");
         std::regex swap("^SWAP HAND WITH OPPONENT$");
 
-        if (std::regex_match(instruction, draw) || std::regex_match(instruction, play) ||
-            std::regex_match(instruction, reverse) || std::regex_match(instruction, swap))
-        {
-            return true;
-        }
-     
-     }
-    
+        return (std::regex_match(instruction, draw) || std::regex_match(instruction, play) ||
+            std::regex_match(instruction, reverse) || std::regex_match(instruction, swap));
+    }
+
     return false;
-    
 }
 
-
-virtual void ActionCard::Print() const override
+void ActionCard::Print() const
 {
-    std::cout << "Type:" << " [" << getType()"]" << std::endl;
-    std::cout << "Point:" << " [" << getInstruction()"]" << std::endl;
+    std::cout << "Type: [" << getType() << "]" << std::endl;
+    std::cout << "Instruction: [" << getInstruction() << "]" << std::endl;
     const int* data = getImageData();
 
     if (data != nullptr)
@@ -57,10 +78,8 @@ virtual void ActionCard::Print() const override
 
         std::cout << "]" << std::endl;
     }
-
     else
     {
         std::cout << "No image data" << std::endl;
     }
 }
-*/
