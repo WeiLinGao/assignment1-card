92b842ff7db35eb23f480c0d0664d678f0c99138
Weilin Gao <136024634+WeiLinGao@users.noreply.github.com>
2023-11-03 20:35:13 -0400

    Update PointCard.cpp

--- PointCard.cpp
+++ PointCard.cpp
@@ -5,30 +5,6 @@
 #include <string>
 #include "Card.hpp"
 
-class PointCard : public Card
-{
-    public:
-        /**
-         * @post: Construct a new Point Card object
-         */
-        PointCard();
-        /**
-         * @return true if the card is playable, false otherwise
-         * For a card to be playable, it has to be drawn and the instruction has to be a valid number
-        */
-        bool isPlayable() override; 
-        /**
-         * @post: Print the Point Card in the following format:
-         * Type: [CardType]
-         * Points: [Instruction]
-         * Card: 
-         * [ImageData]
-         * 
-         * Note: For [ImageData]: If there is no image data, print "No image data" instead
-         */
-        void Print() const override;
-};
-#endif
 
 
 PointCard::PointCard()
