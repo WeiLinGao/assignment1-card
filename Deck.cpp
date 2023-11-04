f1c0a46c0a924840f5993a08c94769950ead9140
Weilin Gao <136024634+WeiLinGao@users.noreply.github.com>
2023-11-04 17:05:18 -0400

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -1,19 +1,20 @@
-
 #include "Deck.hpp"
+#ifndef DECK_CPP
+#define DECK_CPP
+template <typename CardType>
+Deck<CardType>::Deck()
+{
 
-template <typename CardType>
-Deck<CardType>::Deck():cards_{}
-{
-    
-}
-template <typename CardType>
-Deck<CardType>::~Deck()
-{
-    cards_.clear();
 }
 
 template <typename CardType>
-void Deck<CardType>::AddCard(const CardType& card)
+Deck<CardType>::~Deck() 
+{
+
+}
+
+template <typename CardType>
+void Deck<CardType>::AddCard(const CardType& card) 
 {
     cards_.push_back(card);
 }
@@ -21,39 +22,41 @@ void Deck<CardType>::AddCard(const CardType& card)
 template <typename CardType>
 CardType&& Deck<CardType>::Draw() 
 {
-    if (!IsEmpty()) 
+    if (IsEmpty()) 
     {
-        CardType&& card = std::move(cards_.back());
-        cards_.pop_back();
-        return std::move(card);
-    } else 
-    {
-        throw std::out_of_range("EMPTY.");
+        throw std::runtime_error("EMPTY.");
+      
+     
     }
+   
+    CardType&& card = std::move(cards_.back());
+    cards_.pop_back();
+    return std::move(card);
+    
 }
 
 template <typename CardType>
-bool Deck<CardType>::IsEmpty() const
+bool Deck<CardType>::IsEmpty() const 
 {
     return cards_.empty();
 }
 
 template <typename CardType>
-void Deck<CardType>::Shuffle()
+void Deck<CardType>::Shuffle() 
 {
     std::mt19937 rd(2028358904);
     std::shuffle(cards_.begin(), cards_.end(), rd);
 }
 
 template <typename CardType>
-int Deck<CardType>::getSize() const
+int Deck<CardType>::getSize() const 
 {
     return cards_.size();
 }
 
 template <typename CardType>
-std::vector<CardType> Deck<CardType>::getDeck() const
+std::vector<CardType> Deck<CardType>::getDeck() const 
 {
     return cards_;
 }
-
+#endif
